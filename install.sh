cd1cec485f395702ce289156117f3be5e230e20d
rockerbacon <vitor.santos@lab309.com.br>
2024-02-17 09:29:38 -0300

    steamos updates for systems using the DNF package manager

--- install.sh
+++ install.sh
@@ -2,14 +2,20 @@
 
 set -x
 
+mkdir -p /usr/bin/steamos-polkit-helpers
+
 cp etc/polkit-1/rules.d/99-manage-gdm.rules /etc/polkit-1/rules.d/99-manage-gdm.rules
+cp etc/polkit-1/rules.d/99-system-update.rules /etc/polkit-1/rules.d/99-system-update.rules
 
 cp usr/bin/await-gamescope-ready /usr/bin/await-gamescope-ready
 cp usr/bin/load-steam-session-environment /usr/bin/load-steam-session-environment
 cp usr/bin/start-gamescope-compositor /usr/bin/start-gamescope-compositor
 cp usr/bin/start-steam-session /usr/bin/start-steam-session
 cp usr/bin/start-user-session /usr/bin/start-user-session
+cp usr/bin/steamos-polkit-helpers/jupiter-biosupdate /usr/bin/steamos-polkit-helpers/jupiter-biosupdate
+cp usr/bin/steamos-polkit-helpers/steamos-update /usr/bin/steamos-polkit-helpers/steamos-update
 cp usr/bin/steamos-session-select /usr/bin/steamos-session-select
+cp usr/bin/steamos-update /usr/bin/steamos-update
 cp usr/bin/switch-user-session /usr/bin/switch-user-session
 
 cp usr/lib/systemd/user/gamescope-compositor.service /usr/lib/systemd/user/gamescope-compositor.service
